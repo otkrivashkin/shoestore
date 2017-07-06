62d507f89c1baa0a745089b95db50c319bcb0b3a
otkrivashkin <bondarenkoigor1985@gmail.com>
2017-07-06 22:26:52 +0300

    impl index.jsp

--- src/main/java/com/bin/otkrivashkin/controller/PageController.java
+++ src/main/java/com/bin/otkrivashkin/controller/PageController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HomeController {
+public class PageController {
 
 	@RequestMapping("/")
-	public String home() {
-		return "home";
+	public String index() {
+		return "index";
 	}
 }
